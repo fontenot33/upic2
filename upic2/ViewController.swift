626bd66c2fd5ede9db4ffeb5da4659f07360fd74
Ariana Fontenot <arianafontenot@587213269.guest.umich.net>
2015-01-16 20:55:58 -0500

    Printed test output.

--- upic2/ViewController.swift
+++ upic2/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        println("ARIANA ROCKS :D");
         // Do any additional setup after loading the view, typically from a nib.
     }
 
